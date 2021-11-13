cd1373aa62ec7063e51f06bf2352d7fb6ccd0549
zxashik <zxashik@gmail.com>
2021-11-13 22:01:40 +0600

    update all

---
+++ README.md
@@ -0,0 +1,16 @@
+# SunRayes
+
+At SunRayes , we don’t think you should pay a lot for the sunglasses that fit your lifestyle. This is why we carry sunglasses for every occasion and budget. Whether you’re looking for a cool pair of polarized lenses for a short motorcycle ride or a pair of mirrored aviators for your weekend out in the sun, we’ve got you covered.
+
+We strive to put Customer First in our quality & service offering great discounts on a daily basis.
+
+- On our front page, you can buy a product. You can place an order.
+- If you want to buy some product, first you need to log in, after that
+  User can manage their orders. Users can add or delete the order.
+- Admin can add new products, also can add new admin. Users can not access these options.
+- After logging in, you can access the dashboard. In the dashboard, section user can manage their orders.
+- Users can access all products by clicking on the explore page.
+
+## Live Website
+
+link: https://sunglass-zx.web.app/
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const admin = require("firebase-admin");
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const port = process.env.PORT || 5000;
+
+//JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(cors());
 app.use(express.json());
@@ -13,12 +23,172 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4mq9z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
 
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+
+    }
+    next();
+}
 
 async function run() {
     try {
         await client.connect();
-        console.log('database connected successfully');
+
+        const database = client.db('sunglass');
+        const servicesCollection = database.collection('services');
+        const ordersCollection = database.collection('orders');
+        const usersCollection = database.collection('users');
+        const reviewCollection = database.collection('reviews');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            console.log(req.query);
+            const cursor = servicesCollection.find({}, {
+                limit: parseInt(req.query.limit) || 0
+            });
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+
+        //POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+
+            const result = await servicesCollection.insertOne(service);
+            // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+            console.log(result);
+            res.json(result);
+        })
+        //POST API review
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            console.log('hit the post api', review);
+
+            const result = await reviewCollection.insertOne(review);
+            // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+            console.log(result);
+            res.json(result);
+        })
+
+        // GET API review
+        app.get('/reviews', async (req, res) => {
+            console.log(req.query);
+            const cursor = reviewCollection.find({}, {
+                limit: parseInt(req.query.limit) || 0
+            });
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+
+
+        // add to cart
+
+        app.post("/order/create", (req, res) => {
+            console.log(req.body);
+            ordersCollection.insertOne(req.body).then((result) => {
+                res.send(result);
+            })
+        })
+
+        //get my order to my order page
+
+        app.get("/myorders/:userId", async (req, res) => {
+            console.log(req.params.userId);
+
+            const result = await ordersCollection.find({ userId: req.params.userId }).toArray();
+            res.send(result);
+        })
+
+
+
+
+        // DELETE API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // DELETE API order
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+
+        })
+
+        //admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        //post for user registation data
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+        //upsurt for check clone users
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        //admin put
+
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'you do not have access to make admin' })
+            }
+
+        })
+
     }
     finally {
         // await client.close();
